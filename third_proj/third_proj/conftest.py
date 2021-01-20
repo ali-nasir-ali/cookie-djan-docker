6ce32d4356275603e682e25c513cf1d3e0aa0406
anasir514 <anasir514@gmail.com>
2021-01-20 18:43:06 +0000

    initial

---
+++ third_proj/third_proj/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from third_proj.users.models import User
+from third_proj.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
